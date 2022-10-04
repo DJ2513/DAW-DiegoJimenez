69502e3a8c5f41b670f204e58937df22d966780e
DJ2513 <djontiveros2728@gmail.com>
2022-10-04 13:52:25 -0500

    'Tarea_e5'

---
+++ node-test/tareae5/index.js
@@ -0,0 +1,5 @@
+const sh = require('superheroes');
+sh.all;
+const sv = require('supervillains');
+sv.all;
+console.log('Tonight we have the fight of teh century: ' + sh.random() + ' vs ' + sv.random());
