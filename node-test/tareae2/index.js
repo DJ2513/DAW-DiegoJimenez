338c602669c445bf21286da3cd23c9d7d5db09a5
DJ2513 <djontiveros2728@gmail.com>
2022-10-04 13:28:25 -0500

    'Tarea_e3'

--- node-test/tareae2/index.js
+++ node-test/tareae2/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 fs.readFile("file1.txt", (err,data) =>{
     if (!err){
-        console.log('Mensaje: ', data)
+        console.log('Mensaje: ', data);
     }
     else{
         console.log(err);
